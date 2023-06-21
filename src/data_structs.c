47ae9758fe008a2289f0a987030b89b53e391f67
Vignesh RM <136584779+vigneshrm2004@users.noreply.github.com>
2023-06-21 09:39:51 +0000

    added structure for the constraints

--- src/data_structs.c
+++ src/data_structs.c
@@ -80,7 +80,44 @@ typedef struct{
 
 typedef struct{
 	TitlesCons titles;
+	MinSal#define MAX_TITLE_LENGTH 50;
+#define MAX_TITLES 20;
+#define MAX_LOC 5;
+#define MAX_LOCATION_LENGTH 20;
+typedef struct{
+	int min_sal;		//this is score for min sal
+	int score;
+}MinSalaryCons;
+
+typedef struct{
+	int work_hours;		//this is score for working hours
+	int score:
+}MaxHours;
+
+typedef struct{
+	char shift[1];		//this is score for day or night shift
+	int score;
+}ShiftCons;
+
+typedef struct{
+	char titles[MAX_TITLES][MAX_TITLE_LENGTH];
+	int score;
+}TitlesCons;
+
+typedef struct{
+	char loc[MAX_LOC][MAX_LOCATION_LENGTH];	
+	int score;
+}LocCons;
+
+
+
+typedef struct{
+	TitlesCons titles;
 	MinSalaryCons min_sal;
+	ShiftCons shift;
+	LocCons prefferred_location;
+	MaxHours max_hours;
+}AppCons;aryCons min_sal;
 	ShiftCons shift;
 	LocCons prefferred_location;
 	MaxHours max_hours;
