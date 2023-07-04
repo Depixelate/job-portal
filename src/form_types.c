c1d0bffe12900c813df49e1cb0a5aaee903822a4
Depixelate <sukesshvelusamy@gmail.com>
2023-07-04 14:37:18 +0530

    Debugged Dicts, Wrote parse_form I finished debugging and testing the dict types, and have written parse form, but I still have to debug and test it. I also added print methods for the Question type and dicts for debugging, and I created a question_init function to initialize the DictStrStr in the question. I also changed wr.ps1 because the optimization level was -O2, but for debugging -O0 is better.

--- src/form_types.c
+++ src/form_types.c
@@ -1,14 +1,23 @@
 #include "utils.c"
 
 #define MAX_QNO_CHARS 10
-#define MAX_QUESTION_CHARS 200
+#define MAX_PROMPT_CHARS 200
 #define MAX_ANS_KV_PAIRS 10
 
 typedef struct {
     char number[MAX_QNO_CHARS];
-    char question[MAX_QUESTION_CHARS];
-    DictStrStr responses;
+    char prompt[MAX_PROMPT_CHARS];
+    DictStrStr response;
 } Question;
+
+void question_init(Question *q) {
+    dict_str_str_init(&q->response);
+}
+
+void question_print(Question *q) {
+    printf("%s %s: ", q->number, q->prompt);
+    dict_str_str_print(&q->response);
+}
 
 typedef struct {
     char keys[MAX_KV_PAIRS][MAX_KEY_LEN];
@@ -16,7 +25,7 @@ typedef struct {
     int len;
 } DictStrQ;
 
-void dict_str_str_init(DictStrQ *dict) {
+void dict_str_q_init(DictStrQ *dict) {
     dict->len = 0;
 }
 
@@ -26,20 +35,77 @@ void dict_str_q_add(DictStrQ *dict, char *key,  Question *value) {
     dict->len += 1;
 }
 
+void dict_str_q_clear(DictStrQ *dict) {
+    dict->len = 0;
+}
+
 Question *dict_str_q_new_ptr(DictStrQ *dict, char *key) {
     dict->len += 1;
     int new_last_index = dict->len - 1;
     strcpy(dict->keys[new_last_index], key);
+    question_init(&dict->values[new_last_index]);
     return &(dict->values[new_last_index]);
 }
 
+void dict_str_q_next_ptrs(DictStrQ *dict, char **key, Question **value) {
+    dict->len += 1;
+    int last_index = dict->len - 1;
+    *key = dict->keys[last_index];
+    question_init(&dict->values[last_index]);
+    *value = &dict->values[last_index];
+}
+
 //returns true or false depending on if key present
-bool dict_str_q_get(DictStrQ *dict, char *key, Question *value) {
+bool dict_str_q_get(DictStrQ *dict, char *key, Question **value) {
     for(int i = 0; i < dict->len; i++) {
         if (strcmp(dict->keys[i], key) == 0) {
-            value = &dict->values[i];
+            *value = &dict->values[i];
             return true;
         }
     }
     return false;
 }
+
+void dict_str_q_print(DictStrQ *dict) {
+    printf("{\n");
+    for(int i = 0; i < dict->len; i++) {
+        printf("%s: ", dict->keys[i]);
+        question_print(&dict->values[i]);
+    }
+    printf("}\n");
+}
+
+void dict_str_q_test() {
+    DictStrQ dict;
+    Question q1 = {"1.", "hello: ", {{"is it me you're looking for?"}, {""}, 1}}, q2 = {"2.", "goodbye: ", {{"I'm going away"}, {""}, 1}}, q3 = {"3.", "why: ", {{"Come back!"}, {"no"}, 1}};
+    dict_str_q_init(&dict);   
+    dict_str_q_add(&dict, "1.", &q1);
+    Question *value;
+    dict_str_q_get(&dict, "1.", &value);
+    question_print(value);
+    dict_str_q_add(&dict, "2.", &q2);
+    dict_str_q_get(&dict, "2.", &value);
+    question_print(value);
+    dict_str_q_print(&dict);
+    dict_str_q_add(&dict, "3.", &q3);
+    dict_str_q_print(&dict);
+    dict_str_q_get(&dict, "3.", &value);
+    question_print(value);
+    Question *q4 = dict_str_q_new_ptr(&dict, "4.");
+    strcpy(q4->number, "4.");
+    strcpy(q4->prompt, "what do you want?");
+    dict_str_str_add(&q4->response, "I'll be back", "-Arnold Schwarznegger");
+    dict_str_q_print(&dict);
+    char *key;
+    dict_str_q_next_ptrs(&dict, &key, &value);
+    char *response_key, *response_value;
+    dict_str_str_next_ptrs(&(value->response), &response_key, &response_value);
+    printf("Enter q.no, prompt, response key and value: ");
+    scanf(" %[^ ] %[^:]: %[^:]:%[^\n]", value->number, value->prompt, response_key, response_value);
+    strcpy(key, value->number);
+    dict_str_q_print(&dict);
+}
+
+// void main() {
+//     dict_str_q_test();
+// }
--- src/parse_form.c
+++ src/parse_form.c
@@ -1,3 +1,64 @@
 // Check for newline, then check for %d.%d.%d., then read everything upto colon, then read rest of text as key-value pairs until newline %d.%d.%d.%d.
 
- #include "form_types.c"
+#include <string.h>
+#include <ctype.h>
+#include "form_types.c"
+
+#define MAX_LINES 200
+#define MAX_LENGTH 200
+
+bool is_q_begin(char *line, char *q_num, int *question_index) {
+    int tot_chars_read = 0;
+    while(true) {
+        int chars_read = 0;
+        sscanf(line+tot_chars_read, "%[0123456789].%n", q_num + tot_chars_read, &chars_read);
+        if(chars_read == 0) {
+            if(tot_chars_read != 0 && isspace(line[tot_chars_read+1])) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            tot_chars_read += chars_read;
+        }
+    }
+}
+
+typedef enum {
+    READ_QUESTION,
+    READ_RESPONSE
+} Mode;
+
+void parse_form(char lines[MAX_LINES][MAX_LENGTH], int num_lines, DictStrQ *form) {
+    Mode cur_mode = READ_RESPONSE;
+    Question *cur_question = NULL;
+    int cur_line = 0;
+    int cur_line_index = 0;
+    while(cur_line < num_lines) {
+        char new_q_num[MAX_QNO_CHARS] = "";
+        if(cur_mode == READ_RESPONSE) {
+            if(cur_line_index == 0 && is_q_begin(lines[cur_line], new_q_num, cur_line_index)) {
+                cur_question = dict_str_q_new_ptr(form, new_q_num);
+                strcpy(cur_question->number, new_q_num);
+                cur_mode = READ_QUESTION;
+            } else {
+                char *key, *value;
+                dict_str_str_next_ptrs(&(cur_question->response), &key, &value);
+                sscanf(lines[cur_line] + cur_line_index, "%[^:]:%[^\n]", cur_question->response, key, value);
+                cur_line += 1;
+                cur_line_index = 0;
+            }
+        } else if(cur_mode == READ_QUESTION) {
+            char *prompt = cur_question->prompt + strlen(cur_question->prompt);
+            int num_chars_read = 0;
+            sscanf(lines[cur_line] + cur_line_index, "%[^:]:%n", prompt, &num_chars_read);
+            if(num_chars_read == 0) { //no colon
+                cur_line_index = 0;
+                cur_line += 1;
+            } else { // there was a colon
+                cur_line_index += num_chars_read;
+                cur_mode = READ_RESPONSE;
+            }
+        }
+    }
+}
